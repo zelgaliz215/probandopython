72f4836ed0c30c975c9ad20c419eeb37062fd1b5
zelgaliz215 <zelyhobit@gmail.com>
2024-03-17 22:35:47 -0500

    cyper01

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,3 +1,15 @@
 text = 'Hello world'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+""" 
+index = alphabet.find(text[0].lower())
+shifted = alphabet[index + shift]
+print (shifted)
+"""
+encrypted_text =''
+
+for char in text.lower():
+  index = alphabet.find(char)
+  new_index = index + shift
+  encrypted_text += alphabet[new_index]
+  print ('char:', char, 'encrypted_text:', encrypted_text)
